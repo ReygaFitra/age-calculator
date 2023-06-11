0ddcbfb6a14d0d89dde252f90c7a4b6d54f09559
ReygaFitra <regafitra12@gmail.com>
2023-06-11 13:53:34 +0700

    fix:responsive

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,14 @@ function App() {
   const style = {
     main: `w-full h-screen flex justify-center items-center`,
     section: `w-[980px] h-[535px]`,
-    cardBody: `w-full h-full bg-neutralWhite p-20 rounded-3xl border border-neutralLightGrey shadow shadow-sm`,
-    inputBodyStyle: `flex gap-9 w-full h-24 border-b-2 border-neutralLightGrey`,
+    cardBody: `w-[380px] h-[485px] md:w-full md:h-full bg-neutralWhite m-10 md:m-0 p-10 md:p-20 rounded-lg md:rounded-3xl border border-neutralLightGrey shadow shadow-sm`,
+    inputBodyStyle: `flex gap-9 w-full h-40 md:h-28 border-b-2 border-neutralLightGrey`,
     inputContentStyle: `w-full h-full`,
-    inputTitleStyle: `font-bold text-md text-neutralSmokeyGrey uppercase align-center `,
-    inputStyle: `border-neutralLightGrey flex flex-col text-neutralOffBlack font-bold text-2xl px-3 w-1/2 h-10 mt-3 border-2 rounded hover:border-primary focus:ring-2 focus:ring-primary focus:outline-none focus:border-none transition-all`,
+    inputTitleStyle: `font-bold text-lg md:text-md text-neutralSmokeyGrey uppercase align-center `,
+    inputStyle: `border-neutralLightGrey flex flex-col text-neutralOffBlack font-bold text-xl md:text-2xl pl-1 md:px-3 w-full h-16 md:w-1/2 md:h-10 mt-3 border-2 rounded hover:border-primary focus:ring-2 focus:ring-primary focus:outline-none focus:border-none transition-all`,
     inputStyleWarning: `border-warning flex flex-col text-neutralOffBlack font-bold text-2xl px-3 w-1/2 h-10 mt-3 border-2 rounded focus:ring-2 focus:ring-warning focus:outline-none focus:border-none transition-all`,
     AgeResultBodyStyle: `my-16`,
-    AgeResultContentStyle: `flex text-7xl font-bold uppercase italic gap-2 mt-3`,
+    AgeResultContentStyle: `flex text-5xl md:text-7xl font-bold uppercase italic gap-2 mt-3`,
     AgeResultStyle: `text-primary`,
   };
 
